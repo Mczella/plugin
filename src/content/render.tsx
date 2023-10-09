93bac03261a1788a273696e3dff33b20887e9786
Jakub Benes <jakub.benes@outreach.io>
2023-10-09 14:45:03 +0200

    fixed cache

--- src/content/render.tsx
+++ src/content/render.tsx
@@ -1,18 +1,7 @@
 import { createRoot } from "react-dom/client";
-import {CacheProvider} from "@emotion/react";
-import createCache from '@emotion/cache'
-import React from "react";
-import {ChakraProvider} from "@chakra-ui/react";
 
 export function render(content: React.ReactElement) {
-    const container = document.createDocumentFragment();
-    const shadowRoot = this.attachShadow({ mode: 'open' })
-    shadowRoot.appendChild(container);
-    const cache = createCache({
-        key: 'css',
-        container: shadowRoot,
-    });
-    const root = createRoot(container);
-
-    root.render(<CacheProvider value={cache}><ChakraProvider>{content}</ChakraProvider></CacheProvider>);
+  const container = document.createDocumentFragment();
+  const root = createRoot(container);
+  root.render(content);
 }
