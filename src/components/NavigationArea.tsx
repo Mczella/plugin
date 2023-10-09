60160f3041bb1d369a9334084c1d5cfc686fd2ef
Mczella <markaczova@gmail.com>
2023-10-09 13:20:43 +0200

    Separate NavigatioArea component

---
+++ src/components/NavigationArea.tsx
@@ -0,0 +1,37 @@
+import {useEffect, useState} from "react";
+import {ShadowDom} from "./ShadowDom.tsx";
+import {Link} from "@chakra-ui/react";
+import {ButtonComponent} from "./ButtonComponent.tsx";
+
+const NavigationArea = () => {
+    const [parentElement] = useState(() =>
+        document.querySelector(".sectionsItem")
+    );
+
+    const location = window.location.hash
+
+    useEffect(() => {
+        console.log(location)
+    }, [location]);
+
+    return parentElement ? (
+        <ShadowDom parentElement={parentElement}>
+            <div>
+                <Link style={{all: "unset"}} href={"https://www.rohlik.cz/#/pridat-recept"}>
+                    <ButtonComponent
+                        label={"Vytvořit recept"}
+                        icon={"https://www.svgrepo.com/show/303501/red-star-1-logo.svg"}
+                    />
+                </Link>
+                <Link style={{all: "unset"}} href={"https://www.rohlik.cz/#/recepty"}>
+                    <ButtonComponent
+                        label={"Recepty"}
+                        icon={"https://www.svgrepo.com/show/303501/red-star-1-logo.svg"}
+                    />
+                </Link>
+            </div>
+        </ShadowDom>
+    ) : null;
+}
+
+export default NavigationArea
