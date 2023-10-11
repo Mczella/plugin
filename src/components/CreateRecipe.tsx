e784189b914ef91b1cc5cf6823723e0dd587e54b
Mczella <markaczova@gmail.com>
2023-10-11 22:37:41 +0200

    Style search input

--- src/components/CreateRecipe.tsx
+++ src/components/CreateRecipe.tsx
@@ -1,7 +1,8 @@
 import {useQuery} from "@tanstack/react-query";
 import {fetchData, fetchPrices, fetchProducts, fetchStock} from "./Fetch.ts";
-import {Box, Input, Text} from "@chakra-ui/react";
+import {Box, Flex, IconButton, Image, Input, Text} from "@chakra-ui/react";
 import {useState} from "react";
+import {AddIcon} from "@chakra-ui/icons";
 
 const fetchAll = async (query: string) => {
     const productIds = await fetchData(query);
@@ -30,8 +31,9 @@ const fetchAll = async (query: string) => {
 };
 
 const CreateRecipe = () => {
+    // change to RecipeInput
     const [searchQuery, setSearchQuery] = useState("")
-    const {data, isLoading, isError} = useQuery(['data', searchQuery], () => fetchAll(searchQuery));
+    const {data, isError} = useQuery(['data', searchQuery], () => fetchAll(searchQuery));
     console.log(data)
 
     if (isError) {
@@ -40,16 +42,53 @@ const CreateRecipe = () => {
 
     return (
         <Box p={"20px"}>
-            <Input width={"400px"}
+            <Input width={"600px"}
+                   height={"40px"}
+                   rounded={"xl"}
+                   fontSize={"14px"}
+                   color={"rgb(132, 140, 145)"}
+                   outline={"none"}
+                   border={"1px solid rgb(132, 140, 145)"}
                    placeholder={"Vyhledejte suroviny, které chcete přidat do receptu."}
                    value={searchQuery}
                    onChange={(e) => setSearchQuery(e.target.value)}/>
-            {isLoading ?
-                <Text>...</Text>
-                :
-                Object.entries(data).map(([id, product]) => (
-                    <Text key={id}>{product.name}</Text>
-                ))}
+            {data && Object.keys(data).length > 0 ?
+                <Box width={"600px"} border={"1px solid rgb(218, 222, 224)"} rounded={"lg"} boxShadow={"md"}>
+                    {Object.entries(data).map(([id, product]) => (
+                            <Box key={id} width={"600px"}
+                                 borderBottom={
+                                     Object.keys(data).indexOf(id) === Object.keys(data).length - 1
+                                         ? "none"
+                                         : "1px solid rgb(218, 222, 224)"
+                                 }
+                                 p={"10px"}
+                                 _hover={{bg: "gray.100"}}>
+                                <Flex flexDir={"row"} alignItems={"center"} justifyContent={"space-between"}>
+                                    <Flex flexDir={"row"} alignItems={"center"}>
+                                        <Box width={"64px"} height={"64px"} display={"flex"} px={"8px"} py={"8px"}>
+                                            <Image src={product.image} alt="panda" objectFit={"contain"}
+                                                   alignItems={"center"}
+                                                   maxH={"-webkit-fill-available"}/>
+                                        </Box>
+                                        <Flex flexDir={"column"} alignItems={"flex-start"} width={"450px"}>
+                                            <Text fontSize={"13px"} fontWeight={400} mr={"8px"}
+                                                  lineHeight={"22px"}>{product.name}</Text>
+                                            <Text fontSize={"14px"} fontWeight={600}
+                                                  lineHeight={"22px"}>{product.price.amount} {product.price.currency}</Text>
+                                        </Flex>
+                                    </Flex>
+                                    <Box>
+                                        <IconButton aria-label={"vybrat"} height={"40px"} width={"40px"} mr={"12px"}
+                                                    _hover={{border: "1px solid rgb(156, 164, 169)"}}
+                                                    border={"1px solid rgb(218, 222, 224)"} bg={"white"} rounded={"2xl"}
+                                                    icon={<AddIcon color={"black"} boxSize={6}/>}/>
+                                    </Box>
+                                </Flex>
+                            </Box>
+                        )
+                    )}
+                </Box>
+                : null}
         </Box>
 
 
