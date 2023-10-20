fc97ee5bc8d9704c06c36ac114e9c138942c76a1
Mczella <markaczova@gmail.com>
2023-10-20 22:22:08 +0200

    Delete button

--- src/components/NavigationArea.tsx
+++ src/components/NavigationArea.tsx
@@ -1,35 +1,27 @@
-import {useEffect, useState} from "react";
-import {ShadowDom} from "./ShadowDom.tsx";
-import {Flex} from "@chakra-ui/react";
-import {ButtonComponent} from "./ButtonComponent.tsx";
+import { useEffect, useState } from "react";
+import { ShadowDom } from "./ShadowDom.tsx";
+import { ButtonComponent } from "./ButtonComponent.tsx";
 
 const NavigationArea = () => {
-    const [parentElement] = useState(() =>
-        document.querySelector(".sectionsItem")
-    );
+  const [parentElement] = useState(() =>
+    document.querySelector(".sectionsItem"),
+  );
 
-    const location = window.location.hash
+  const location = window.location.hash;
 
-    useEffect(() => {
-        console.log(location)
-    }, [location]);
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
 
-    return parentElement ? (
-        <ShadowDom parentElement={parentElement}>
-            <Flex flexDir={"row"}>
-                    <ButtonComponent
-                        navigate={"/#/pridat-recept"}
-                        label={"Vytvořit recept"}
-                        icon={"https://www.svgrepo.com/show/303501/red-star-1-logo.svg"}
-                    />
-                    <ButtonComponent
-                        navigate={"/#/recepty"}
-                        label={"Recepty"}
-                        icon={"https://www.svgrepo.com/show/303501/red-star-1-logo.svg"}
-                    />
-            </Flex>
-        </ShadowDom>
-    ) : null;
-}
+  return parentElement ? (
+    <ShadowDom parentElement={parentElement}>
+      <ButtonComponent
+        navigate={"/#/recepty"}
+        label={"Recepty"}
+        icon={"https://www.svgrepo.com/show/303501/red-star-1-logo.svg"}
+      />
+    </ShadowDom>
+  ) : null;
+};
 
-export default NavigationArea
+export default NavigationArea;
