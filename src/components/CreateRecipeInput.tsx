4c2c0bf830c0146f0502bd058c74f3d287cee1ee
Mczella <markaczova@gmail.com>
2023-10-20 22:21:52 +0200

    Delete fragment

--- src/components/CreateRecipeInput.tsx
+++ src/components/CreateRecipeInput.tsx
@@ -4,9 +4,9 @@ import {
   Dispatch,
   FC,
   SetStateAction,
+  useEffect,
   useRef,
   useState,
-  useEffect,
 } from "react";
 
 interface Product {
@@ -88,138 +88,137 @@ const CreateRecipeInput: FC<Props> = ({
     }));
     console.log(selectedProducts);
   };
+  console.log(isDropdownOpen);
 
   return (
-    <>
-      <Flex alignItems={"center"} flexDir={"column"}>
-        <Box
-          bg={"rgb(242, 244, 244)"}
-          py={"32px"}
-          mb={"48px"}
-          rounded={"3xl"}
-          display={"flex"}
-          justifyContent={"center"}
-          width="-webkit-fill-available"
-          zIndex={0}
-        >
-          <Box ref={dropdownRef}>
-            <Input
+    <Flex alignItems={"center"} flexDir={"column"}>
+      <Box
+        bg={"rgb(242, 244, 244)"}
+        py={"32px"}
+        mb={"48px"}
+        rounded={"3xl"}
+        display={"flex"}
+        justifyContent={"center"}
+        width="-webkit-fill-available"
+        zIndex={0}
+      >
+        <Box ref={dropdownRef}>
+          <Input
+            width={"600px"}
+            onFocus={handleFocus}
+            height={"40px"}
+            rounded={"xl"}
+            fontSize={"14px"}
+            bg={"white"}
+            color={"rgb(132, 140, 145)"}
+            outline={"none"}
+            border={"1px solid rgb(132, 140, 145)"}
+            placeholder={
+              "Vyhledejte produkty, které odpovídají vaším požadavkům na danou ingredienci."
+            }
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          {productsByIds &&
+          Object.keys(productsByIds).length > 0 &&
+          isDropdownOpen ? (
+            //close on outside click
+            <Box
               width={"600px"}
-              onFocus={handleFocus}
-              height={"40px"}
-              rounded={"xl"}
-              fontSize={"14px"}
+              maxHeight={"480px"}
+              overflow="scroll"
+              sx={{
+                "::-webkit-scrollbar": {
+                  color: "#007 #bada55",
+                },
+              }}
+              border={"1px solid rgb(218, 222, 224)"}
+              rounded={"lg"}
+              boxShadow={"md"}
+              position={"absolute"}
               bg={"white"}
-              color={"rgb(132, 140, 145)"}
-              outline={"none"}
-              border={"1px solid rgb(132, 140, 145)"}
-              placeholder={
-                "Vyhledejte produkty, které odpovídají vaším požadavkům na danou ingredienci."
-              }
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            {productsByIds &&
-            Object.keys(productsByIds).length > 0 &&
-            isDropdownOpen ? (
-              //close on outside click
-              <Box
-                width={"600px"}
-                maxHeight={"480px"}
-                overflow="scroll"
-                sx={{
-                  "::-webkit-scrollbar": {
-                    color: "#007 #bada55",
-                  },
-                }}
-                border={"1px solid rgb(218, 222, 224)"}
-                rounded={"lg"}
-                boxShadow={"md"}
-                position={"absolute"}
-                bg={"white"}
-              >
-                {productIds.map((productId) => {
-                  console.log(productsByIds, productId);
-                  const product = productsByIds[productId];
-                  return (
-                    <Box
-                      key={productId}
-                      width={"600px"}
-                      borderBottom={
-                        productIds.indexOf(productId) === productIds.length - 1
-                          ? "none"
-                          : "1px solid rgb(218, 222, 224)"
-                      }
-                      p={"10px"}
-                      _hover={{ bg: "gray.100" }}
+            >
+              {productIds.map((productId) => {
+                console.log(productsByIds, productId);
+                const product = productsByIds[productId];
+                return (
+                  <Box
+                    key={productId}
+                    width={"600px"}
+                    borderBottom={
+                      productIds.indexOf(productId) === productIds.length - 1
+                        ? "none"
+                        : "1px solid rgb(218, 222, 224)"
+                    }
+                    p={"10px"}
+                    _hover={{ bg: "gray.100" }}
+                  >
+                    <Flex
+                      flexDir={"row"}
+                      alignItems={"center"}
+                      justifyContent={"space-between"}
                     >
-                      <Flex
-                        flexDir={"row"}
-                        alignItems={"center"}
-                        justifyContent={"space-between"}
-                      >
-                        <Flex flexDir={"row"} alignItems={"center"}>
-                          <Box
-                            width={"64px"}
-                            height={"64px"}
-                            display={"flex"}
-                            px={"8px"}
-                            py={"8px"}
-                          >
-                            <Image
-                              src={product.image}
-                              alt="panda"
-                              objectFit={"contain"}
-                              alignItems={"center"}
-                              maxH={"-webkit-fill-available"}
-                            />
-                          </Box>
-                          <Flex
-                            flexDir={"column"}
-                            alignItems={"flex-start"}
-                            width={"450px"}
-                          >
-                            <Text
-                              fontSize={"13px"}
-                              fontWeight={400}
-                              mr={"8px"}
-                              lineHeight={"22px"}
-                            >
-                              {product.name}
-                            </Text>
-                            <Text
-                              fontSize={"14px"}
-                              fontWeight={600}
-                              lineHeight={"22px"}
-                            >
-                              {product.price.amount} {product.price.currency}
-                            </Text>
-                          </Flex>
-                        </Flex>
-                        <Box>
-                          <IconButton
-                            aria-label={"vybrat"}
-                            height={"40px"}
-                            width={"40px"}
-                            mr={"12px"}
-                            _hover={{ border: "1px solid rgb(156, 164, 169)" }}
-                            border={"1px solid rgb(218, 222, 224)"}
-                            bg={"white"}
-                            rounded={"2xl"}
-                            icon={<AddIcon color={"black"} boxSize={6} />}
-                            onClick={() => handleAddToRecipe(product)}
+                      <Flex flexDir={"row"} alignItems={"center"}>
+                        <Box
+                          width={"64px"}
+                          height={"64px"}
+                          display={"flex"}
+                          px={"8px"}
+                          py={"8px"}
+                        >
+                          <Image
+                            src={product.image}
+                            alt="panda"
+                            objectFit={"contain"}
+                            alignItems={"center"}
+                            maxH={"-webkit-fill-available"}
                           />
                         </Box>
+                        <Flex
+                          flexDir={"column"}
+                          alignItems={"flex-start"}
+                          width={"450px"}
+                        >
+                          <Text
+                            fontSize={"13px"}
+                            fontWeight={400}
+                            mr={"8px"}
+                            lineHeight={"22px"}
+                          >
+                            {product.name}
+                          </Text>
+                          <Text
+                            fontSize={"14px"}
+                            fontWeight={600}
+                            lineHeight={"22px"}
+                          >
+                            {product.price.amount} {product.price.currency}
+                          </Text>
+                        </Flex>
                       </Flex>
-                    </Box>
-                  );
-                })}
-              </Box>
-            ) : null}
-          </Box>
+                      <Box>
+                        <IconButton
+                          aria-label={"vybrat"}
+                          height={"40px"}
+                          width={"40px"}
+                          mr={"12px"}
+                          _hover={{ border: "1px solid rgb(156, 164, 169)" }}
+                          border={"1px solid rgb(218, 222, 224)"}
+                          bg={"white"}
+                          rounded={"2xl"}
+                          icon={<AddIcon color={"black"} boxSize={6} />}
+                          onClick={() => handleAddToRecipe(product)}
+                        />
+                      </Box>
+                    </Flex>
+                  </Box>
+                );
+              })}
+            </Box>
+          ) : null}
         </Box>
-      </Flex>
-    </>
+      </Box>
+    </Flex>
   );
 };
 
