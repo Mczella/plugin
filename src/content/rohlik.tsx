0f3e3585625b3d0774b6eea60b95a690d787ee63
Jakub Benes <jakub.benes@outreach.io>
2023-10-24 10:22:05 +0200

    fix modal

--- src/content/rohlik.tsx
+++ src/content/rohlik.tsx
@@ -1,5 +1,12 @@
 import { Rohlik } from "../components/Rohlik";
 import { render } from "./render";
-import {HashRouter as Router} from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
-render(<Router><Rohlik /></Router>);
+// TODO: It looks that if we bootstrap the app too fast, the original client gets confused because hydration doesn't match.
+setTimeout(() => {
+  render(
+    <Router>
+      <Rohlik />
+    </Router>
+  );
+}, 500);
