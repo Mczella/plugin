f82936f457d1dabdb5ee9eb7f7020e2a15303a86
Mczella <markaczova@gmail.com>
2023-10-28 23:07:13 +0200

    Add globalstateprovider

--- src/content/rohlik.tsx
+++ src/content/rohlik.tsx
@@ -1,12 +1,12 @@
 import { Rohlik } from "../components/Rohlik";
+import { GlobalStateProvider } from "../components/StoreProvider";
 import { render } from "./render";
 import { HashRouter as Router } from "react-router-dom";
 
-// TODO: It looks that if we bootstrap the app too fast, the original client gets confused because hydration doesn't match.
-setTimeout(() => {
-  render(
-    <Router>
-      <Rohlik />
-    </Router>
-  );
-}, 500);
+render(
+    <GlobalStateProvider>
+        <Router>
+            <Rohlik />
+        </Router>
+    </GlobalStateProvider>
+);
