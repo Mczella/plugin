f351e37f1315436dbb94856e42421b64ce251b81
Mczella <markaczova@gmail.com>
2023-10-30 20:05:56 +0100

    Separate into two components

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,10 +1,13 @@
-import { Button, Flex, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import { Flex, Grid, GridItem } from "@chakra-ui/react";
 import Add from "./Add.tsx";
 import { useMyStore } from "./store.tsx";
+import RecipeComponent from "./RecipeComponent.tsx";
+import { Fragment } from "react";
 
 const Recipes = () => {
-  const { recipes } = useMyStore();
+  const { recipes, recipesInCart } = useMyStore();
 
+  console.log("receptyvkosiku", recipesInCart);
   return (
     <Flex flexDir={"column"}>
       <Grid templateColumns="repeat(7, 1fr)" gap="10px" m="3%">
@@ -13,43 +16,10 @@ const Recipes = () => {
         </GridItem>
       </Grid>
       <Grid templateColumns="repeat(7, 1fr)" gap="10px" m="3%">
-        {recipes.map((item, index) => (
-          <GridItem
-            key={index}
-            display="flex"
-            flexDir="column"
-            alignItems="center"
-          >
-            <Image
-              src={item.image}
-              fallbackSrc={
-                "https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-7.jpg"
-              }
-              alt="panda"
-              width="100%"
-            />
-            <Text height="3em" isTruncated>
-              {item.name}
-            </Text>
-            <Text fontSize="24px" fontWeight="bold" lineHeight="1.4">
-              100 Kč
-            </Text>
-            <Text fontSize="12px" lineHeight={1.4}>
-              20,00 Kč/porce
-            </Text>
-            <Button
-              mt="10px"
-              bg="white"
-              color="black"
-              border="1px solid rgba(0, 0, 0, 0.15)"
-              height="32px"
-              display="flex"
-              alignItems="center"
-              _hover={{ bg: "green" }}
-            >
-              Do košíku
-            </Button>
-          </GridItem>
+        {recipes.map((recipe) => (
+          <Fragment key={recipe.id}>
+            <RecipeComponent recipe={recipe} />
+          </Fragment>
         ))}
       </Grid>
     </Flex>
