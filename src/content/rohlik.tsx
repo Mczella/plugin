782c63996655bfa7d2299faf8683f820dc5545a8
Mczella <markaczova@gmail.com>
2023-11-03 08:44:40 +0100

    Move StoreProvider to store folder

--- src/content/rohlik.tsx
+++ src/content/rohlik.tsx
@@ -1,12 +1,12 @@
 import { Rohlik } from "../components/Rohlik";
-import { GlobalStateProvider } from "../components/StoreProvider";
+import { GlobalStateProvider } from "../components/store/StoreProvider.tsx";
 import { render } from "./render";
 import { HashRouter as Router } from "react-router-dom";
 
 render(
-    <GlobalStateProvider>
-        <Router>
-            <Rohlik />
-        </Router>
-    </GlobalStateProvider>
+  <GlobalStateProvider>
+    <Router>
+      <Rohlik />
+    </Router>
+  </GlobalStateProvider>,
 );
