c4ad9725f0052508a3ab5a0cb6ef1de423128ca6
Mczella <markaczova@gmail.com>
2023-11-03 09:01:50 +0100

    Fix styles

--- src/components/recipes/RecipeComponent.tsx
+++ src/components/recipes/RecipeComponent.tsx
@@ -1,4 +1,4 @@
-import { Button, GridItem, Image, Text } from "@chakra-ui/react";
+import { Box, Button, GridItem, Image, Text } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
 import { useMyStore } from "../store/store.tsx";
 import { NewRecipe } from "../types.ts";
@@ -19,39 +19,43 @@ const RecipeComponent: FC<Props> = ({ recipe }) => {
 
   return (
     <GridItem display="flex" flexDir="column" alignItems="center">
-      <Image
-        src={recipe.image}
-        fallbackSrc="https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-7.jpg"
-        alt="panda"
-        width="100%"
-      />
-      <Text height="3em" isTruncated casing={"capitalize"}>
-        {recipe.name}
-      </Text>
-      <Text fontSize="24px" fontWeight="bold" lineHeight="1.4">
-        {totalPrice === 0 ? "Vyprodáno" : `${Math.ceil(totalPrice)} Kč`}
-      </Text>
-      <Text fontSize="12px" lineHeight={1.4}>
-        {`${Math.ceil(pricePerPortion)} Kč/porce`}
-      </Text>
-      {recipesInCart.includes(recipe.id) ? (
-        <AddIcon />
-      ) : (
-        <Button
-          mt="10px"
-          bg="white"
-          color="black"
-          border="1px solid rgba(0, 0, 0, 0.15)"
-          height="32px"
-          display="flex"
-          alignItems="center"
-          isDisabled={totalPrice === 0}
-          _hover={{ bg: "green" }}
-          onClick={() => addRecipeToCart(recipe.id)}
-        >
-          Do košíku
-        </Button>
-      )}
+      <Box w={"165px"}>
+        <Box w={"150px"} h={"150px"}>
+          <Image
+            src={recipe.image}
+            fallbackSrc="https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-7.jpg"
+            alt="panda"
+            width="100%"
+          />
+        </Box>
+        <Text height="3em" isTruncated casing={"capitalize"}>
+          {recipe.name}
+        </Text>
+        <Text fontSize="24px" fontWeight="bold" lineHeight="1.4">
+          {totalPrice === 0 ? "Vyprodáno" : `${Math.ceil(totalPrice)} Kč`}
+        </Text>
+        <Text fontSize="12px" lineHeight={1.4}>
+          {`${Math.ceil(pricePerPortion)} Kč/porce`}
+        </Text>
+        {recipesInCart.includes(recipe.id) ? (
+          <AddIcon />
+        ) : (
+          <Button
+            mt="10px"
+            bg="white"
+            color="black"
+            border="1px solid rgba(0, 0, 0, 0.15)"
+            height="32px"
+            display="flex"
+            alignItems="center"
+            isDisabled={totalPrice === 0}
+            _hover={{ bg: "green" }}
+            onClick={() => addRecipeToCart(recipe.id)}
+          >
+            Do košíku
+          </Button>
+        )}
+      </Box>
     </GridItem>
   );
 };
