bd9829d7f4d7ad6dbac82448776a657ed4851f27
Mczella <markaczova@gmail.com>
2023-11-04 20:24:52 +0100

    Add second modal step and useEffect

--- src/components/ingredients/CreateIngredientModal.tsx
+++ src/components/ingredients/CreateIngredientModal.tsx
@@ -9,6 +9,11 @@ import {
   ModalCloseButton,
   ModalContent,
   ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
   Text,
 } from "@chakra-ui/react";
 import {
@@ -64,6 +69,9 @@ const CreateIngredientModal: FC<Props> = ({
     fetchAll(searchQuery),
   );
   const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
+  const [step, setStep] = useState<number>(1);
+  const [selectedIngredient, setSelectedIngredient] =
+    useState<NewIngredient | null>(null);
 
   const { productsByIds, productIds } = data ?? {
     productIds: [],
@@ -76,10 +84,10 @@ const CreateIngredientModal: FC<Props> = ({
 
   const handleIngredientClick = (ingredient: NewIngredient) => {
     setSelectedIngredients([...selectedIngredients, ingredient]);
-    const updatedIngredients = ingredients.filter(
-      (item) => !selectedIngredients.includes(item) && item !== ingredient,
-    );
-    setCurrentIngredients(updatedIngredients);
+    // const updatedIngredients = ingredients.filter(
+    //   (item) => !selectedIngredients.includes(item) && item !== ingredient,
+    // );
+    // setCurrentIngredients(updatedIngredients);
   };
 
   const handleSave = () => {
@@ -101,11 +109,24 @@ const CreateIngredientModal: FC<Props> = ({
       setSelectedProducts([]);
       setSearchQuery("");
       setName(null);
+      setStep(1);
       onClose();
     } else if (name != null && selectedProducts.length === 0) {
       setError(true);
     }
   };
+
+  useEffect(() => {
+    const filteredIngredients = ingredients.filter(
+      (ingredient) =>
+        !selectedIngredients.some(
+          (selected) => selected.id === ingredient.id,
+        ) &&
+        (!selectedIngredient || selectedIngredient.id !== ingredient.id),
+    );
+
+    setCurrentIngredients(filteredIngredients);
+  }, [selectedIngredients, selectedIngredient]);
 
   useEffect(() => {
     if (selectedProducts.length > 0) {
@@ -133,129 +154,260 @@ const CreateIngredientModal: FC<Props> = ({
               justifyContent={"center"}
               alignItems={"center"}
             >
-              <Heading
-                fontSize={"24px"}
-                fontWeight={900}
-                lineHeight={"32px"}
-                mb={"24px"}
-              >
-                Přidat ingredienci
-              </Heading>
-              <FormControl isRequired>
-                <AutoComplete
-                  openOnFocus
-                  closeOnSelect
-                  creatable
-                  onCreateOption={({ item }: { item: { value: string } }) => {
-                    setName(item.value);
-                    setShowInput(true);
-                  }}
-                >
-                  <AutoCompleteInput
-                    width={"740px"}
-                    height={"40px"}
-                    rounded={"xl"}
-                    fontSize={"14px"}
-                    bg={"white"}
-                    color={"rgb(132, 140, 145)"}
-                    outline={"none"}
-                    mb={"24px"}
-                    border={"1px solid rgb(132, 140, 145)"}
-                    placeholder="Vyberte z vašich ingrediencí..."
-                  />
-                  <AutoCompleteList
-                    border={"1px solid rgb(218, 222, 224)"}
-                    rounded={"lg"}
-                    boxShadow={"md"}
-                    bg={"white"}
-                  >
-                    {currentIngredients.map((ingredient) => (
-                      <AutoCompleteItem
-                        _hover={{ bg: "gray.100" }}
-                        key={ingredient.id}
-                        value={ingredient.name}
-                        textTransform="capitalize"
-                        onClick={() => {
-                          handleIngredientClick(ingredient);
-                          onClose();
-                        }}
-                      >
-                        {ingredient.name}
-                      </AutoCompleteItem>
-                    ))}
-                    <AutoCompleteCreatable>
-                      {({ value }) => (
-                        <Text>
-                          Vytvořit ingredienci s názvem{" "}
-                          <Text as={"b"}>{value}</Text>.
-                        </Text>
-                      )}
-                    </AutoCompleteCreatable>
-                  </AutoCompleteList>
-                </AutoComplete>
-              </FormControl>
-              {showInput ? (
+              {step === 1 ? (
                 <>
-                  <CreateIngredientInput
-                    searchQuery={searchQuery}
-                    setSearchQuery={setSearchQuery}
-                    productsByIds={productsByIds}
-                    productIds={productIds}
-                    selectedProducts={selectedProducts}
-                    setSelectedProducts={setSelectedProducts}
-                  />
-                  <ChosenProducts
-                    selectedProducts={selectedProducts}
-                    setSelectedProducts={setSelectedProducts}
-                  />
+                  <Heading
+                    fontSize={"24px"}
+                    fontWeight={900}
+                    lineHeight={"32px"}
+                    mb={"24px"}
+                  >
+                    Přidat ingredienci
+                  </Heading>
+                  <FormControl isRequired>
+                    <AutoComplete
+                      openOnFocus
+                      closeOnSelect
+                      creatable
+                      onCreateOption={({
+                        item,
+                      }: {
+                        item: { value: string };
+                      }) => {
+                        setName(item.value);
+                        setShowInput(true);
+                      }}
+                    >
+                      <AutoCompleteInput
+                        width={"740px"}
+                        height={"40px"}
+                        rounded={"xl"}
+                        fontSize={"14px"}
+                        bg={"white"}
+                        color={"rgb(132, 140, 145)"}
+                        outline={"none"}
+                        mb={"24px"}
+                        border={"1px solid rgb(132, 140, 145)"}
+                        placeholder="Vyberte z vašich ingrediencí..."
+                      />
+                      <AutoCompleteList
+                        border={"1px solid rgb(218, 222, 224)"}
+                        rounded={"lg"}
+                        boxShadow={"md"}
+                        bg={"white"}
+                      >
+                        {currentIngredients.map((ingredient) => (
+                          <AutoCompleteItem
+                            _hover={{ bg: "gray.100" }}
+                            key={ingredient.id}
+                            value={ingredient.name}
+                            textTransform="capitalize"
+                            onClick={() => setSelectedIngredient(ingredient)}
+                          >
+                            {ingredient.name}
+                          </AutoCompleteItem>
+                        ))}
+                        <AutoCompleteCreatable>
+                          {({ value }) => (
+                            <Text>
+                              Vytvořit ingredienci s názvem{" "}
+                              <Text as={"b"}>{value}</Text>.
+                            </Text>
+                          )}
+                        </AutoCompleteCreatable>
+                      </AutoCompleteList>
+                    </AutoComplete>
+                  </FormControl>
+                  {showInput ? (
+                    <>
+                      <CreateIngredientInput
+                        searchQuery={searchQuery}
+                        setSearchQuery={setSearchQuery}
+                        productsByIds={productsByIds}
+                        productIds={productIds}
+                        selectedProducts={selectedProducts}
+                        setSelectedProducts={setSelectedProducts}
+                      />
+                      <ChosenProducts
+                        selectedProducts={selectedProducts}
+                        setSelectedProducts={setSelectedProducts}
+                      />
+                    </>
+                  ) : selectedIngredient ? (
+                    <Text>{selectedIngredient.name}</Text>
+                  ) : null}
                 </>
-              ) : null}
+              ) : (
+                <>
+                  <Heading
+                    fontSize={"24px"}
+                    fontWeight={900}
+                    lineHeight={"32px"}
+                    mb={"24px"}
+                  >
+                    Zadejte množství vybrané ingredience pro tento recept
+                  </Heading>
+                  <NumberInput
+                    min={0.1}
+                    max={30}
+                    width={"600px"}
+                    height={"40px"}
+                    isRequired
+                  >
+                    <NumberInputField
+                      id={"amount"}
+                      placeholder={"Zadejte množství"}
+                      type={"number"}
+                      height={"40px"}
+                      rounded={"xl"}
+                      fontSize={"14px"}
+                      bg={"white"}
+                      color={"rgb(132, 140, 145)"}
+                      outline={"none"}
+                      border={"1px solid rgb(132, 140, 145)"}
+                    />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </>
+              )}
               {error ? (
                 <Text mt={"24px"} color={"red"}>
                   Vyberte alespoň jeden produkt.
                 </Text>
               ) : null}
-              <ButtonGroup
-                mt={"40px"}
-                display={"flex"}
-                justifyContent={"center"}
-                gap={"5px"}
-              >
-                <Button
-                  bg="white"
-                  color="black"
-                  fontSize={"14px"}
-                  fontWeight={"600"}
-                  border="1px solid rgba(0, 0, 0, 0.15)"
-                  height="40px"
-                  display="flex"
-                  alignItems="center"
-                  rounded={"xl"}
-                  onClick={() => {
-                    setSearchQuery("");
-                    setSelectedProducts([]);
-                    setShowInput(false);
-                    setName(null);
-                    onClose();
-                  }}
+
+              {step === 1 ? (
+                <ButtonGroup
+                  mt={"40px"}
+                  display={"flex"}
+                  justifyContent={"center"}
+                  gap={"5px"}
                 >
-                  Zrušit
-                </Button>
-                <Button
-                  height={"40px"}
-                  w={"360px"}
-                  bg={"rgb(109, 163, 5)"}
-                  fontSize={"14px"}
-                  fontWeight={"600"}
-                  rounded={"xl"}
-                  boxShadow={"md"}
-                  color={"white"}
-                  _hover={{ bg: "rgb(87, 130, 4)" }}
-                  onClick={handleSave}
+                  <Button
+                    bg="white"
+                    color="black"
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    border="1px solid rgba(0, 0, 0, 0.15)"
+                    height="40px"
+                    display="flex"
+                    alignItems="center"
+                    rounded={"xl"}
+                    onClick={() => {
+                      setSearchQuery("");
+                      setSelectedProducts([]);
+                      setSelectedIngredient(null);
+                      setShowInput(false);
+                      setName(null);
+                      onClose();
+                    }}
+                  >
+                    Zrušit
+                  </Button>
+                  <Button
+                    bg="white"
+                    color="black"
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    border="1px solid rgba(0, 0, 0, 0.15)"
+                    height="40px"
+                    display="flex"
+                    alignItems="center"
+                    rounded={"xl"}
+                    isDisabled
+                  >
+                    Zpět
+                  </Button>
+                  <Button
+                    height={"40px"}
+                    w={"360px"}
+                    bg={"rgb(109, 163, 5)"}
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    rounded={"xl"}
+                    boxShadow={"md"}
+                    color={"white"}
+                    _hover={{ bg: "rgb(87, 130, 4)" }}
+                    onClick={() => setStep(2)}
+                  >
+                    Pokračovat
+                  </Button>
+                </ButtonGroup>
+              ) : (
+                <ButtonGroup
+                  mt={"40px"}
+                  display={"flex"}
+                  justifyContent={"center"}
+                  gap={"5px"}
                 >
-                  Vytvořit
-                </Button>
-              </ButtonGroup>
+                  <Button
+                    bg="white"
+                    color="black"
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    border="1px solid rgba(0, 0, 0, 0.15)"
+                    height="40px"
+                    display="flex"
+                    alignItems="center"
+                    rounded={"xl"}
+                    onClick={
+                      selectedIngredient
+                        ? () => {
+                            setSelectedIngredient(null);
+                            onClose();
+                          }
+                        : () => {
+                            setSearchQuery("");
+                            setSelectedProducts([]);
+                            setShowInput(false);
+                            setName(null);
+                            onClose();
+                          }
+                    }
+                  >
+                    Zrušit
+                  </Button>
+                  <Button
+                    bg="white"
+                    color="black"
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    border="1px solid rgba(0, 0, 0, 0.15)"
+                    height="40px"
+                    display="flex"
+                    alignItems="center"
+                    rounded={"xl"}
+                    onClick={() => setStep(1)}
+                  >
+                    Zpět
+                  </Button>
+                  <Button
+                    height={"40px"}
+                    w={"360px"}
+                    bg={"rgb(109, 163, 5)"}
+                    fontSize={"14px"}
+                    fontWeight={"600"}
+                    rounded={"xl"}
+                    boxShadow={"md"}
+                    color={"white"}
+                    _hover={{ bg: "rgb(87, 130, 4)" }}
+                    onClick={
+                      selectedIngredient
+                        ? () => {
+                            handleIngredientClick(selectedIngredient);
+                            onClose();
+                            setStep(1);
+                          }
+                        : handleSave
+                    }
+                  >
+                    Vytvořit
+                  </Button>
+                </ButtonGroup>
+              )}
               {/*<Button onClick={() => purgeStorage()}>Purge Storage</Button>*/}
               {/*bytesInUse: {bytesInUse}*/}
             </Flex>
