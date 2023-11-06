9f48a9cb4c855970091779310d1f8a3eccd3eed4
Jakub Benes <jakub.benes@outreach.io>
2023-11-06 16:21:38 +0100

    hakovani routeru

--- src/components/MainArea.tsx
+++ src/components/MainArea.tsx
@@ -1,18 +1,55 @@
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { ShadowDom } from "./ShadowDom.tsx";
 import CreateRecipe from "./recipes/CreateRecipe.tsx";
 import Recipes from "./recipes/Recipes.tsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import CheckRecipes from "./cart/CheckRecipes.tsx";
 
 const MainArea = () => {
   const [parentElement] = useState(() =>
-    document.querySelector("#pageFullWidth"),
+    document.querySelector<HTMLElement>("#pageFullWidth")
   );
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    let oldHref: null | string = null;
+    const body = document.querySelector("body");
+
+    const observer = new MutationObserver(() => {
+      const { pathname } = new URL(document.location.href);
+
+      if (pathname !== oldHref || oldHref === null) {
+        oldHref = pathname;
+        if (pathname !== "/") {
+          navigate("/");
+          console.log("naviguji na defaultni route, kde nic nerenderujeme...");
+        }
+      }
+    });
+
+    if (body) {
+      observer.observe(body, { childList: true, subtree: true });
+    }
+
+    return () => observer.disconnect();
+  }, [navigate]);
+
+  useLayoutEffect(() => {
+    // Google Analytics
+    if (!parentElement) {
+      return;
+    }
+
+    parentElement.style.display = location.pathname === "/" ? "block" : "none";
+  }, [location, parentElement]);
 
   return parentElement ? (
     <ShadowDom parentElement={parentElement}>
       <Routes>
+        {/* / is placeholder for empty url, where we don't render anything */}
+        <Route path="/" element={<></>} />
         <Route path="/pridat-recept/:id" element={<CreateRecipe />} />
         <Route path="/recepty" element={<Recipes />} />
         <Route path="/prehled-receptu" element={<CheckRecipes />} />
--- src/content/rohlik.tsx
+++ src/content/rohlik.tsx
@@ -3,10 +3,12 @@ import { GlobalStateProvider } from "../components/store/StoreProvider.tsx";
 import { render } from "./render";
 import { HashRouter as Router } from "react-router-dom";
 
-render(
-  <GlobalStateProvider>
-    <Router>
-      <Rohlik />
-    </Router>
-  </GlobalStateProvider>,
-);
+setTimeout(() => {
+  render(
+    <GlobalStateProvider>
+      <Router>
+        <Rohlik />
+      </Router>
+    </GlobalStateProvider>
+  );
+}, 1000);
