fde16138cbabf9122ec9c50984a1d3775d57256a
Mczella <markaczova@gmail.com>
2023-11-10 20:58:27 +0100

    handleSave for different use cases and add button modals

--- src/components/ingredients/IngredientModal.tsx
+++ src/components/ingredients/IngredientModal.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  ButtonGroup,
   Flex,
   Modal,
   ModalBody,
@@ -10,68 +9,118 @@ import {
 import { FC, RefObject, useRef, useState } from "react";
 import {
   useMyStore,
+  usePurgeStorage,
   // usePurgeStorage,
 } from "../store/store.tsx";
 import { SimpleIngredient, NewIngredient } from "../types.ts";
 import IngredientModalOne from "./IngredientModalOne.tsx";
 import IngredientModalTwo from "./IngredientModalTwo.tsx";
+import IngredientInRecipeButtons from "./IngredientInRecipeButtons.tsx";
+import IngredientButtons from "./IngredientButtons.tsx";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
   focusRef: RefObject<HTMLInputElement>;
+  type: "create" | "createInRecipe" | "edit" | "editInRecipe";
+  id?: NewIngredient["id"];
 };
 
-const IngredientModal: FC<Props> = ({ isOpen, onClose, focusRef }) => {
+const IngredientModal: FC<Props> = ({
+  isOpen,
+  onClose,
+  focusRef,
+  type,
+  id,
+}) => {
   const {
     addIngredient,
     addToSelectedIngredients,
     selectedProducts,
-    selectedIngredient,
     editSelectedProducts,
     selectIngredient,
+    name,
+    editAmount,
+    amount,
+    ingredients,
+    editIngredients,
+    editSelectedIngredients,
+    editName,
   } = useMyStore();
-  // const purgeStorage = usePurgeStorage();
+  const purgeStorage = usePurgeStorage();
   // // const bytesInUse = useBytesInUse();
-  const [name, setName] = useState<string | null>(null);
-  const [amount, setAmount] = useState<number>(0);
   const modalContainer = useRef(null);
   const [step, setStep] = useState<number>(1);
 
   const modalReset = () => {
     setStep(1);
-    setAmount(0);
+    editAmount(0);
     editSelectedProducts([]);
     selectIngredient(null);
     onClose();
+    editName(null);
   };
 
-  const handleIngredientClick = (ingredient: NewIngredient) => {
-    const newSelectedIngredient: NewIngredient = {
-      ...ingredient,
-      amount: amount,
-    };
-    addToSelectedIngredients(newSelectedIngredient);
-    modalReset();
-  };
-
+  console.log(type);
   const handleSave = () => {
-    if (name != null && selectedProducts.length > 0) {
-      const updatedSelectedProducts: SimpleIngredient[] = selectedProducts.map(
-        (product) => {
-          const { id, preferred } = product;
-          return { id, preferred };
-        },
-      );
+    const updatedSelectedProducts: SimpleIngredient[] = selectedProducts.map(
+      (product) => {
+        const { id, preferred } = product;
+        return { id, preferred };
+      },
+    );
+    if (
+      name != null &&
+      selectedProducts.length > 0 &&
+      type === "createInRecipe"
+    ) {
       const newIngredient: NewIngredient = {
+        name: name,
+        selectedProducts: updatedSelectedProducts,
+        id: Date.now().toString(36),
+      };
+      addIngredient(newIngredient);
+
+      const newRecipeIngredient: NewIngredient = {
         name: name,
         selectedProducts: updatedSelectedProducts,
         id: Date.now().toString(36),
         amount: amount,
       };
-      addToSelectedIngredients(newIngredient);
-      addIngredient(newIngredient);
+      addToSelectedIngredients(newRecipeIngredient);
+
       modalReset();
+    } else if (
+      name != null &&
+      selectedProducts.length > 0 &&
+      type === "editInRecipe"
+    ) {
+      const editedIngredients = ingredients.map((ingredient) => {
+        if (ingredient.id === id) {
+          return {
+            ...ingredient,
+            name: name,
+            selectedProducts: updatedSelectedProducts,
+          };
+        }
+        return ingredient;
+      });
+
+      editIngredients(editedIngredients);
+
+      const editedRecipeIngredients = ingredients.map((ingredient) => {
+        if (ingredient.id === id) {
+          return {
+            ...ingredient,
+            name: name,
+            selectedProducts: updatedSelectedProducts,
+            amount: amount,
+          };
+        }
+        return ingredient;
+      });
+
+      editSelectedIngredients(editedRecipeIngredients);
     }
   };
 
@@ -96,134 +145,29 @@ const IngredientModal: FC<Props> = ({ isOpen, onClose, focusRef }) => {
               justifyContent={"center"}
               alignItems={"center"}
             >
-              {step === 1 ? (
-                <IngredientModalOne setName={setName} />
-              ) : (
-                <IngredientModalTwo setAmount={setAmount} />
-              )}
+              {step === 1 ? <IngredientModalOne /> : <IngredientModalTwo />}
 
-              {step === 1 ? (
-                <ButtonGroup
-                  mt={"40px"}
-                  display={"flex"}
-                  justifyContent={"center"}
-                  gap={"5px"}
-                >
-                  <Button
-                    bg="white"
-                    color="black"
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    border="1px solid rgba(0, 0, 0, 0.15)"
-                    height="40px"
-                    display="flex"
-                    alignItems="center"
-                    rounded={"xl"}
-                    onClick={() => {
-                      modalReset();
-                    }}
-                  >
-                    Zrušit
-                  </Button>
-                  <Button
-                    bg="white"
-                    color="black"
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    border="1px solid rgba(0, 0, 0, 0.15)"
-                    height="40px"
-                    display="flex"
-                    alignItems="center"
-                    rounded={"xl"}
-                    isDisabled
-                  >
-                    Zpět
-                  </Button>
-                  <Button
-                    height={"40px"}
-                    w={"360px"}
-                    bg={"rgb(109, 163, 5)"}
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    rounded={"xl"}
-                    boxShadow={"md"}
-                    color={"white"}
-                    isDisabled={
-                      !selectedIngredient && selectedProducts.length === 0
-                    }
-                    _hover={{ bg: "rgb(87, 130, 4)" }}
-                    onClick={() => setStep(2)}
-                  >
-                    Pokračovat
-                  </Button>
-                </ButtonGroup>
+              {type === "editInRecipe" || type === "createInRecipe" ? (
+                <IngredientInRecipeButtons
+                  step={step}
+                  setStep={setStep}
+                  modalReset={modalReset}
+                  handleSave={handleSave}
+                />
               ) : (
-                <ButtonGroup
-                  mt={"40px"}
-                  display={"flex"}
-                  justifyContent={"center"}
-                  gap={"5px"}
-                >
-                  <Button
-                    bg="white"
-                    color="black"
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    border="1px solid rgba(0, 0, 0, 0.15)"
-                    height="40px"
-                    display="flex"
-                    alignItems="center"
-                    rounded={"xl"}
-                    onClick={() => modalReset()}
-                  >
-                    Zrušit
-                  </Button>
-                  <Button
-                    bg="white"
-                    color="black"
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    border="1px solid rgba(0, 0, 0, 0.15)"
-                    height="40px"
-                    display="flex"
-                    alignItems="center"
-                    rounded={"xl"}
-                    onClick={() => setStep(1)}
-                  >
-                    Zpět
-                  </Button>
-                  <Button
-                    height={"40px"}
-                    w={"360px"}
-                    bg={"rgb(109, 163, 5)"}
-                    fontSize={"14px"}
-                    fontWeight={"600"}
-                    rounded={"xl"}
-                    boxShadow={"md"}
-                    color={"white"}
-                    isDisabled={amount === 0}
-                    _hover={{ bg: "rgb(87, 130, 4)" }}
-                    onClick={() => {
-                      selectedIngredient && amount > 0
-                        ? handleIngredientClick(selectedIngredient)
-                        : handleSave();
-                    }}
-                  >
-                    Vytvořit
-                  </Button>
-                </ButtonGroup>
+                <IngredientButtons
+                  modalReset={modalReset}
+                  handleSave={handleSave}
+                />
               )}
-              {/*<Button*/}
-              {/*  onClick={() => {*/}
-              {/*    resetProducts();*/}
-              {/*    resetIngredient();*/}
-              {/*    resetIngredients();*/}
-              {/*    purgeStorage();*/}
-              {/*  }}*/}
-              {/*>*/}
-              {/*  Purge Storage*/}
-              {/*</Button>*/}
-              {/*/!*bytesInUse: {bytesInUse}*!/*/}
+              <Button
+                onClick={() => {
+                  purgeStorage();
+                }}
+              >
+                Purge Storage
+              </Button>
+              {/*bytesInUse: {bytesInUse}*/}
             </Flex>
           </ModalBody>
         </ModalContent>
