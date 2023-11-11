a0d145aee1f21ce18a6e7f42f03e168a9014fa0c
Mczella <markaczova@gmail.com>
2023-11-11 17:50:48 +0100

    Add amount units

--- src/components/ingredients/IngredientModalTwo.tsx
+++ src/components/ingredients/IngredientModalTwo.tsx
@@ -1,15 +1,18 @@
 import {
   Heading,
+  HStack,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
+  Text,
 } from "@chakra-ui/react";
 import { useMyStore } from "../store/store.tsx";
 
 const IngredientModalTwo = () => {
-  const { editAmount, amount } = useMyStore();
+  const { editAmount, amount, selectedProducts } = useMyStore();
+  console.log(selectedProducts);
   return (
     <>
       <Heading
@@ -20,32 +23,35 @@ const IngredientModalTwo = () => {
       >
         Zadejte množství vybrané ingredience pro tento recept
       </Heading>
-      <NumberInput
-        min={0.1}
-        max={3000}
-        width={"600px"}
-        height={"40px"}
-        defaultValue={amount ? amount : 0}
-        isRequired
-        onChange={(_, valueAsNumber) => editAmount(valueAsNumber)}
-      >
-        <NumberInputField
-          id={"amount"}
-          placeholder={"Zadejte množství"}
-          type={"number"}
+      <HStack>
+        <NumberInput
+          min={0.1}
+          max={3000}
+          width={"600px"}
           height={"40px"}
-          rounded={"xl"}
-          fontSize={"14px"}
-          bg={"white"}
-          color={"rgb(132, 140, 145)"}
-          outline={"none"}
-          border={"1px solid rgb(132, 140, 145)"}
-        />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInput>
+          defaultValue={amount ? amount : 0}
+          isRequired
+          onChange={(_, valueAsNumber) => editAmount(valueAsNumber)}
+        >
+          <NumberInputField
+            id={"amount"}
+            placeholder={"Zadejte množství"}
+            type={"number"}
+            height={"40px"}
+            rounded={"xl"}
+            fontSize={"14px"}
+            bg={"white"}
+            color={"rgb(132, 140, 145)"}
+            outline={"none"}
+            border={"1px solid rgb(132, 140, 145)"}
+          />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+        <Text>{selectedProducts[1]?.packageInfo?.unit}</Text>
+      </HStack>
     </>
   );
 };
