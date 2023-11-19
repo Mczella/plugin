b1d5d66a4c44533ee62041130d03aa5b78f9cf99
Mczella <markaczova@gmail.com>
2023-11-19 19:11:00 +0100

    Add PlusMinus and getamount function

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -16,9 +16,11 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 import Ingredient from "../recipes/Ingredient.tsx";
 import IngredientModal from "../ingredients/IngredientModal.tsx";
+import PlusMinus from "../PlusMinus.tsx";
+import { NewIngredient } from "../types.ts";
 
 const Products = () => {
-  const { ingredients } = useMyStore();
+  const { ingredients, ingredientsInCart } = useMyStore();
   // const [productArray, setProductArray] = useState<Product["id"][]>([]);
   const { isOpen: isOpen, onOpen: onOpen, onClose: onClose } = useDisclosure();
   const focusRef = useRef<HTMLInputElement>(null);
@@ -26,108 +28,143 @@ const Products = () => {
   const { state } = useLocation();
   const navigate = useNavigate();
 
+  const getAmountInCart = (ingredient: NewIngredient): number => {
+    return ingredientsInCart.reduce((totalAmount, ingredientInCart) => {
+      const matchingProduct = ingredient.selectedProducts.find(
+        (product) => product.id === ingredientInCart.id,
+      );
+
+      if (matchingProduct) {
+        const amount = Math.ceil(
+          ingredientInCart.amount / ingredientInCart.packageAmount,
+        );
+        return totalAmount + amount;
+      }
+
+      return totalAmount;
+    }, 0);
+  };
+
   return (
-    <Box
-      pt={"16px"}
-      pb={"12px"}
-      mx={"calc(3% + 16px)"}
-      w={"1000px"}
-      minH={"100vh"}
-      mb={"30px"}
-    >
-      <BreadcrumbNav type={"produkty"} />
-      <Flex flexDir={"column"}>
-        <Heading
-          pt={"12px"}
-          fontSize={"36px"}
-          fontWeight={900}
-          lineHeight={"48px"}
-        >
-          Produkty
-        </Heading>
-        <Flex flexDir={"row"} alignItems={"center"} gap={"10px"} mt={"32px"}>
-          <Badge
-            bg={"rgb(109, 163, 5)"}
-            rounded={"2xl"}
-            fontSize={"12px"}
-            fontWeight={400}
-            lineHeight={"22px"}
-            px={"8px"}
-            variant={"solid"}
-            color={"white"}
+    <>
+      <Box
+        pt={"16px"}
+        pb={"12px"}
+        mx={"calc(3% + 16px)"}
+        w={"1000px"}
+        minH={"100vh"}
+        mb={"30px"}
+      >
+        <BreadcrumbNav type={"produkty"} />
+        <Flex flexDir={"column"}>
+          <Heading
+            pt={"12px"}
+            fontSize={"36px"}
+            fontWeight={900}
+            lineHeight={"48px"}
           >
-            <Text casing={"capitalize"}>Nové</Text>
-          </Badge>
-          <Heading fontSize={"20px"} fontWeight={900} lineHeight={"30px"}>
-            Seznam vámi vytvořených ingrediencí
+            Produkty
           </Heading>
-          <Icon color={"rgb(28, 37, 41)"} boxSize={"20px"} />
-        </Flex>
-        <Text
-          mt={"8px"}
-          mb={"16px"}
-          fontSize={"16px"}
-          fontWeight={400}
-          lineHeight={"24px"}
-        >
-          Nakupte jednotlivé produkty s jistotou nejlepší ceny.
-        </Text>
-        <Grid templateColumns="repeat(7, 1fr)" gap="10px">
-          <GridItem display="flex" flexDir="column" alignItems="center">
-            <Add
-              text={"Přidat ingredienci"}
-              type={"pridat-ingredienci"}
-              onOpen={onOpen}
-            />
-          </GridItem>
-          <GridItem display="flex" flexDir="column" alignItems="center">
-            <Box onClick={() => navigate("/recepty")}>
-              <Add text={"Recepty"} type={"odkaz"} />
-            </Box>
-          </GridItem>
-        </Grid>
-        {state && (
-          <Text mt={"24px"} color={"red"}>
-            {state.error}
+          <Flex flexDir={"row"} alignItems={"center"} gap={"10px"} mt={"32px"}>
+            <Badge
+              bg={"rgb(109, 163, 5)"}
+              rounded={"2xl"}
+              fontSize={"12px"}
+              fontWeight={400}
+              lineHeight={"22px"}
+              px={"8px"}
+              variant={"solid"}
+              color={"white"}
+            >
+              <Text casing={"capitalize"}>Nové</Text>
+            </Badge>
+            <Heading fontSize={"20px"} fontWeight={900} lineHeight={"30px"}>
+              Seznam vámi vytvořených ingrediencí
+            </Heading>
+            <Icon color={"rgb(28, 37, 41)"} boxSize={"20px"} />
+          </Flex>
+          <Text
+            mt={"8px"}
+            mb={"16px"}
+            fontSize={"16px"}
+            fontWeight={400}
+            lineHeight={"24px"}
+          >
+            Nakupte jednotlivé produkty s jistotou nejlepší ceny.
           </Text>
-        )}
-        <Heading
-          mt={"24px"}
-          mb={"24px"}
-          fontSize={"24px"}
-          fontWeight={"normal"}
-        >
-          Vaše produkty
-        </Heading>
-        {/*<RadioGroup onChange={setValue} value={value}>*/}
-        {/*  <Stack direction="row" gap={"16px"}>*/}
-        {/*    <Radio value="1" fontSize={"12px"}>*/}
-        {/*      OD NEJNOVĚJŠÍHO*/}
-        {/*    </Radio>*/}
-        {/*    <Radio value="2" fontSize={"12px"}>*/}
-        {/*      OD NEJLEVNĚJŠÍHO*/}
-        {/*    </Radio>*/}
-        {/*    <Radio value="3" fontSize={"12px"}>*/}
-        {/*      OD NEJDRAŽŠÍHO*/}
-        {/*    </Radio>*/}
-        {/*    <Radio value="4" fontSize={"12px"}>*/}
-        {/*      OD NEJLEVNĚJŠÍHO ZA JEDNOTKU*/}
-        {/*    </Radio>*/}
-        {/*  </Stack>*/}
-        {/*</RadioGroup>*/}
-        <Grid templateColumns="repeat(5, 1fr)" gap="10px">
-          {ingredients.map((ingredient) => (
-            <Ingredient key={ingredient.id} ingredient={ingredient} />
-          ))}
-        </Grid>
-      </Flex>
-      <IngredientModal
-        focusRef={focusRef}
-        isOpen={isOpen}
-        onClose={onClose}
-        type={"create"}
-      />
-    </Box>
+          <Grid templateColumns="repeat(7, 1fr)" gap="10px">
+            <GridItem display="flex" flexDir="column" alignItems="center">
+              <Add
+                text={"Přidat ingredienci"}
+                type={"pridat-ingredienci"}
+                onOpen={onOpen}
+              />
+            </GridItem>
+            <GridItem display="flex" flexDir="column" alignItems="center">
+              <Box onClick={() => navigate("/recepty")}>
+                <Add text={"Recepty"} type={"odkaz"} />
+              </Box>
+            </GridItem>
+          </Grid>
+          {state && (
+            <Text mt={"24px"} color={"red"}>
+              {state.error}
+            </Text>
+          )}
+          <Heading
+            mt={"24px"}
+            mb={"24px"}
+            fontSize={"24px"}
+            fontWeight={"normal"}
+          >
+            Vaše produkty
+          </Heading>
+          {/*<RadioGroup onChange={setValue} value={value}>*/}
+          {/*  <Stack direction="row" gap={"16px"}>*/}
+          {/*    <Radio value="1" fontSize={"12px"}>*/}
+          {/*      OD NEJNOVĚJŠÍHO*/}
+          {/*    </Radio>*/}
+          {/*    <Radio value="2" fontSize={"12px"}>*/}
+          {/*      OD NEJLEVNĚJŠÍHO*/}
+          {/*    </Radio>*/}
+          {/*    <Radio value="3" fontSize={"12px"}>*/}
+          {/*      OD NEJDRAŽŠÍHO*/}
+          {/*    </Radio>*/}
+          {/*    <Radio value="4" fontSize={"12px"}>*/}
+          {/*      OD NEJLEVNĚJŠÍHO ZA JEDNOTKU*/}
+          {/*    </Radio>*/}
+          {/*  </Stack>*/}
+          {/*</RadioGroup>*/}
+          <Grid templateColumns="repeat(5, 1fr)" gap="10px">
+            {ingredients.map((ingredient) => {
+              const amount = getAmountInCart(ingredient);
+
+              return (
+                <Flex
+                  flexDir={"column"}
+                  alignItems={"center"}
+                  mb={"20px"}
+                  key={ingredient.id}
+                >
+                  <Ingredient ingredient={ingredient} />
+                  <PlusMinus
+                    amount={amount}
+                    handleAdd={() => console.log("h")}
+                    handleSubtract={() => console.log("h")}
+                  />
+                </Flex>
+              );
+            })}
+          </Grid>
+        </Flex>
+        <IngredientModal
+          focusRef={focusRef}
+          isOpen={isOpen}
+          onClose={onClose}
+          type={"create"}
+        />
+      </Box>
+    </>
   );
 };
 
