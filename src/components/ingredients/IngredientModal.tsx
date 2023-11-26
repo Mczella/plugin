2c7f9a3c9d40e641eed11b272fd6ba9b8859a22d
Mczella <markaczova@gmail.com>
2023-11-26 20:47:09 +0100

    Refactor handleSave

--- src/components/ingredients/IngredientModal.tsx
+++ src/components/ingredients/IngredientModal.tsx
@@ -10,11 +10,7 @@ import {
   useMyStore,
   // usePurgeStorage,
 } from "../store/store.tsx";
-import {
-  SimpleIngredient,
-  NewIngredient,
-  NewRecipeIngredient,
-} from "../types.ts";
+import { NewIngredient } from "../types.ts";
 import IngredientModalOne from "./IngredientModalOne.tsx";
 import IngredientModalTwo from "./IngredientModalTwo.tsx";
 import IngredientInRecipeButtons from "./IngredientInRecipeButtons.tsx";
@@ -72,108 +68,99 @@ const IngredientModal: FC<Props> = ({
 
   console.log(type);
   const handleSave = () => {
-    const updatedSelectedProducts: SimpleIngredient[] = selectedProducts.map(
-      (product) => {
-        const { id, preferred } = product;
-        return { id, preferred };
-      },
-    );
-    if (
-      name != null &&
-      selectedProducts.length > 0 &&
-      type === "createInRecipe"
-    ) {
-      const createdId = Date.now().toString(36);
-      const newIngredient: NewIngredient = {
-        name: name,
-        selectedProducts: updatedSelectedProducts,
-        id: createdId,
-        unit: selectedProducts[0].unit,
-        optimize: optimize,
-        sortBy: sortBy,
-      };
-      addIngredient(newIngredient);
+    if (name != null && selectedProducts.length > 0) {
+      const updatedSelectedProducts = selectedProducts.map(
+        ({ id, preferred }) => ({ id, preferred }),
+      );
 
-      const newRecipeIngredient: NewRecipeIngredient = {
-        name: name,
-        selectedProducts: updatedSelectedProducts,
-        id: createdId,
-        amount: amount,
-        optimize: optimize,
-        sortBy: sortBy,
-        unit: selectedProducts[0].unit,
-      };
-      addToSelectedIngredients(newRecipeIngredient);
+      switch (type) {
+        case "createInRecipe":
+          const createdIdCreate = Date.now().toString(36);
+          const newIngredientCreateInRecipe = {
+            name,
+            selectedProducts: updatedSelectedProducts,
+            id: createdIdCreate,
+            unit: selectedProducts[0].unit,
+            optimize,
+            sortBy,
+          };
+          addIngredient(newIngredientCreateInRecipe);
+
+          const newRecipeIngredientCreate = {
+            name,
+            selectedProducts: updatedSelectedProducts,
+            id: createdIdCreate,
+            amount,
+            optimize,
+            sortBy,
+            unit: selectedProducts[0].unit,
+          };
+          addToSelectedIngredients(newRecipeIngredientCreate);
+          break;
+
+        case "editInRecipe":
+          const editedIngredientsEditInRecipe = ingredients.map((ingredient) =>
+            ingredient.id === id
+              ? {
+                  ...ingredient,
+                  name,
+                  selectedProducts: updatedSelectedProducts,
+                  optimize,
+                  sortBy,
+                }
+              : ingredient,
+          );
+
+          editIngredients(editedIngredientsEditInRecipe);
+
+          const editedRecipeIngredientsEdit = selectedIngredients.map(
+            (ingredient) =>
+              ingredient.id === id
+                ? {
+                    ...ingredient,
+                    name,
+                    selectedProducts: updatedSelectedProducts,
+                    amount,
+                    optimize,
+                    sortBy,
+                  }
+                : ingredient,
+          );
+
+          editSelectedIngredients(editedRecipeIngredientsEdit);
+          break;
+
+        case "edit":
+          const editedIngredientsEdit = ingredients.map((ingredient) =>
+            ingredient.id === id
+              ? {
+                  ...ingredient,
+                  name,
+                  selectedProducts: updatedSelectedProducts,
+                  optimize,
+                  sortBy,
+                }
+              : ingredient,
+          );
+
+          editIngredients(editedIngredientsEdit);
+          break;
+
+        case "create":
+          const newIngredientCreate = {
+            name,
+            selectedProducts: updatedSelectedProducts,
+            id: Date.now().toString(36),
+            unit: selectedProducts[0].unit,
+            optimize,
+            sortBy,
+          };
+          addIngredient(newIngredientCreate);
+          break;
+      }
 
       modalReset();
-    } else if (
-      name != null &&
-      selectedProducts.length > 0 &&
-      type === "editInRecipe"
-    ) {
-      const editedIngredients = ingredients.map((ingredient) => {
-        if (ingredient.id === id) {
-          return {
-            ...ingredient,
-            name: name,
-            selectedProducts: updatedSelectedProducts,
-            optimize: optimize,
-            sortBy: sortBy,
-          };
-        }
-        return ingredient;
-      });
-
-      editIngredients(editedIngredients);
-
-      const editedRecipeIngredients: NewRecipeIngredient[] =
-        selectedIngredients.map((ingredient) => {
-          if (ingredient.id === id) {
-            return {
-              ...ingredient,
-              name: name,
-              selectedProducts: updatedSelectedProducts,
-              amount: amount,
-              optimize: optimize,
-              sortBy: sortBy,
-            };
-          }
-          return ingredient;
-        });
-
-      editSelectedIngredients(editedRecipeIngredients);
-    } else if (name != null && selectedProducts.length > 0 && type === "edit") {
-      const editedIngredients = ingredients.map((ingredient) => {
-        if (ingredient.id === id) {
-          return {
-            ...ingredient,
-            name: name,
-            selectedProducts: updatedSelectedProducts,
-            optimize: optimize,
-            sortBy: sortBy,
-          };
-        }
-        return ingredient;
-      });
-
-      editIngredients(editedIngredients);
-    } else if (
-      name != null &&
-      selectedProducts.length > 0 &&
-      type === "create"
-    ) {
-      const newIngredient: NewIngredient = {
-        name: name,
-        selectedProducts: updatedSelectedProducts,
-        id: Date.now().toString(36),
-        unit: selectedProducts[0].unit,
-        optimize: optimize,
-        sortBy: sortBy,
-      };
-      addIngredient(newIngredient);
     }
-
-    modalReset();
   };
 
   return (
