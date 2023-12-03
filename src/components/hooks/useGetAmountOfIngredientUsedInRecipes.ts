f3e09dfd004d296ebb4ef793ab92703a3a1bebeb
Mczella <markaczova@gmail.com>
2023-12-03 21:45:39 +0100

    Fix

--- src/components/hooks/useGetAmountOfIngredientUsedInRecipes.ts
+++ src/components/hooks/useGetAmountOfIngredientUsedInRecipes.ts
@@ -1,32 +1,38 @@
 import { useMyStore } from "../store/store.tsx";
 import { NewIngredient } from "../types.ts";
+import { useGetIngredientPrice } from "./useGetIngredientPrice.tsx";
 
 export const useGetAmountOfIngredientUsedInRecipes = (
   ingredient: NewIngredient,
 ) => {
   const { recipesInCart, recipes } = useMyStore();
-  const amountNeeded = recipesInCart.map((recipe) => {
-    const recipeInCart = recipes.find(
-      (originalRecipe) => recipe.id === originalRecipe.id,
-    );
+  const { productInfo } = useGetIngredientPrice(ingredient);
 
-    if (recipeInCart) {
-      const matchingIngredient = recipeInCart.ingredients.find(
-        (ing) => ing.id === ingredient.id,
+  if (productInfo.length === 0) {
+    return;
+  }
+
+  return Math.ceil(
+    recipesInCart.reduce((acc, recipeInCart) => {
+      const matchingRecipe = recipes.find(
+        (originalRecipe) => recipeInCart.id === originalRecipe.id,
       );
-      if (matchingIngredient) {
-        return Math.ceil(matchingIngredient.amount * recipe.amount);
+
+      if (matchingRecipe) {
+        const matchingIngredient = matchingRecipe.ingredients.find(
+          (ing) => ing.id === ingredient.id,
+        );
+
+        if (matchingIngredient) {
+          return (
+            acc +
+            (matchingIngredient.amount * recipeInCart.amount) /
+              productInfo[0].packageInfo.amount
+          );
+        }
       }
-    }
 
-    return null;
-  });
-
-  return amountNeeded.reduce((acc: number, current) => {
-    if (current !== null) {
-      return acc + current;
-    } else {
       return acc;
-    }
-  }, 0);
+    }, 0),
+  );
 };
