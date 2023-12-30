4e1d2ff4901d6423c615a9b35fe5529baef89ed9
Mczella <markaczova@gmail.com>
2023-12-30 21:26:55 +0100

    New component CartHeader handling cart price

---
+++ src/components/cart/CartHeader.tsx
@@ -0,0 +1,22 @@
+import { useGetCartPrice } from "../hooks/useGetCartPrice.ts";
+import { useEffect } from "react";
+import { useMyStore } from "../store/store.tsx";
+
+export const CartHeader = () => {
+  const { ingredientsInCart } = useMyStore();
+
+  const cartPrice = useGetCartPrice(ingredientsInCart);
+  const cartHeaderPrice = document.querySelector('[data-test="headerPrice"]');
+
+  useEffect(() => {
+    if (cartHeaderPrice && cartPrice) {
+      const textContent = cartHeaderPrice.textContent;
+      const editedContent = textContent?.split("Kč")[0].trim() + " Kč";
+      cartHeaderPrice.textContent = `${editedContent} + ${cartPrice.totalPrice.toFixed(
+        1,
+      )} Kč`;
+    }
+  }, [cartHeaderPrice, cartPrice]);
+
+  return false;
+};
