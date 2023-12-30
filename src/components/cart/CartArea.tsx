1d226b7ed06de0783f1b31daf5a5455ac5fc5ad4
Mczella <markaczova@gmail.com>
2023-12-30 21:26:39 +0100

    Remove useEffect and add new component CartHeader

--- src/components/cart/CartArea.tsx
+++ src/components/cart/CartArea.tsx
@@ -7,12 +7,11 @@ import { useParentElement } from "../hooks/useParentElement.ts";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import IngredientInCart from "./IngredientInCart.tsx";
+import { CartHeader } from "./CartHeader.tsx";
 
 const CartArea = () => {
   const location = useLocation();
   const { ingredientsInCart, recipesInCart } = useMyStore();
-
-  console.log(location);
 
   const cart = useParentElement(
     document.querySelector('[data-test*="categoryCart"]'),
@@ -23,11 +22,14 @@ const CartArea = () => {
 
   const parentElement = cart ? cart : empty ? empty : null;
 
-  // useEffect(() => {
-  //   if ((empty && ingredientsInCart.length > 0) || recipesInCart.length > 0) {
-  //     empty.style.display = "none";
-  //   }
-  // }, [empty, ingredientsInCart.length, recipesInCart.length]);
+  useEffect(() => {
+    if (
+      (empty && ingredientsInCart.length > 0) ||
+      (empty && recipesInCart.length > 0)
+    ) {
+      empty.style.display = "none";
+    }
+  }, [empty, ingredientsInCart.length, recipesInCart.length]);
 
   const cartHeaderElement = document.querySelector('[data-test="cart-header"]');
   const cartMain = document.querySelector('[data-test="cart"]');
@@ -37,74 +39,65 @@ const CartArea = () => {
     const mouseOverEvent = new MouseEvent("mouseover", {
       bubbles: true,
     });
+    const mouseleave = new MouseEvent("mouseout", {
+      bubbles: true,
+    });
     if (cartHeaderElement && location.pathname != "/") {
       cartHeaderElement.dispatchEvent(mouseOverEvent);
-    }
-
-    if (cartElement && location.pathname != "/") {
-      console.log("cart");
-      cartElement.className = "";
-      cartElement.style.display = "flex";
-    }
-    if (cartHeaderElement && location.pathname != "/") {
-      cartHeaderElement?.addEventListener("mouseleave", (e) => {
-        if (cartElement) {
-          cartElement.className = "";
-          cartElement.style.display = "flex";
-          cartElement.style.flexDirection = "column";
-        }
-      });
-      if (cartElement && location.pathname != "/") {
-        cartElement?.addEventListener("mouseleave", (e) => {
-          if (cartElement) {
-            cartElement.className = "";
-            cartElement.style.display = "flex";
-            cartElement.style.flexDirection = "column";
-          }
-        });
-      }
+      cartHeaderElement.dispatchEvent(mouseleave);
     }
   }, [cartElement, cartHeaderElement, cartMain, location]);
 
   const cartProductsElement: HTMLElement | null = document.querySelector(
     '[class*="cartProducts"]',
   );
+
   useEffect(() => {
     if (cartProductsElement) {
       cartProductsElement.style.overflow = "scroll";
     }
   }, [cartProductsElement]);
 
-  return parentElement && recipesInCart.length > 0 ? (
+  return (parentElement && recipesInCart.length > 0) ||
+    ingredientsInCart.length > 0 ? (
     <ShadowDom parentElement={parentElement}>
-      <Text
-        color={"rgb(93, 103, 108)"}
-        bg={"rgb(242, 244, 244)"}
-        fontSize={"13px"}
-        fontWeight={"700"}
-        letterSpacing={"-0.4px"}
-        px={"16px"}
-        py={"8px"}
-      >
-        Recepty
-      </Text>
-      {recipesInCart.map((recipe) => (
-        <RecipeInCart key={recipe.id} recipe={recipe} />
-      ))}
-      <Text
-        color={"rgb(93, 103, 108)"}
-        bg={"rgb(242, 244, 244)"}
-        fontSize={"13px"}
-        fontWeight={"700"}
-        letterSpacing={"-0.4px"}
-        px={"16px"}
-        py={"8px"}
-      >
-        Ingredience
-      </Text>
-      {ingredientsInCart.map((ingredient) => (
-        <IngredientInCart key={ingredient.id} ingredient={ingredient} />
-      ))}
+      {recipesInCart.length > 0 ? (
+        <>
+          <CartHeader />
+          <Text
+            color={"rgb(93, 103, 108)"}
+            bg={"rgb(242, 244, 244)"}
+            fontSize={"13px"}
+            fontWeight={"700"}
+            letterSpacing={"-0.4px"}
+            px={"16px"}
+            py={"8px"}
+          >
+            Recepty
+          </Text>
+          {recipesInCart.map((recipe) => (
+            <RecipeInCart key={recipe.id} recipe={recipe} />
+          ))}
+        </>
+      ) : null}
+      {ingredientsInCart.length > 0 ? (
+        <>
+          <Text
+            color={"rgb(93, 103, 108)"}
+            bg={"rgb(242, 244, 244)"}
+            fontSize={"13px"}
+            fontWeight={"700"}
+            letterSpacing={"-0.4px"}
+            px={"16px"}
+            py={"8px"}
+          >
+            Ingredience
+          </Text>
+          {ingredientsInCart.map((ingredient) => (
+            <IngredientInCart key={ingredient.id} ingredient={ingredient} />
+          ))}
+        </>
+      ) : null}
       <CartButton />
     </ShadowDom>
   ) : null;
