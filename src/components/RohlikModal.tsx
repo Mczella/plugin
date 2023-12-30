e91134743dcb8362704be964dcd5b5bde52bfa16
Mczella <markaczova@gmail.com>
2023-12-30 21:52:38 +0100

    Add overflow hidden to body and edit visibility until loaded

--- src/components/RohlikModal.tsx
+++ src/components/RohlikModal.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FC, SetStateAction } from "react";
+import { Dispatch, FC, SetStateAction, useEffect } from "react";
 import { useParentElement } from "./hooks/useParentElement.ts";
 import { useOutsideClick } from "./hooks/useOutsideClick.ts";
 import "./rohlikModal.css";
@@ -24,7 +24,17 @@ export const RohlikModal: FC<Props> = ({
     setIsModalOpen(null);
   });
 
-  function modifyModal(iframe: HTMLIFrameElement) {
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isModalOpen]);
+
+  const modifyModal = (iframe: HTMLIFrameElement) => {
     const productDetailElement: HTMLElement | undefined | null =
       iframe.contentDocument?.getElementById("productDetail");
 
@@ -35,13 +45,15 @@ export const RohlikModal: FC<Props> = ({
       ?.querySelector('[data-test="notification-wrapper"]')
       ?.remove();
     iframe.contentDocument?.querySelector('[data-test="topPanel"]')?.remove();
+    iframe.contentDocument?.querySelectorAll(".Toastify");
+
     iframe.contentDocument?.getElementById("header")?.remove();
     productDetailParent.style.padding = "0px";
     if (productDetailElement) {
       productDetailElement.style.border = "none";
       productDetailElement.style.boxShadow = "none";
     }
-  }
+  };
 
   console.log(modalRef);
 
@@ -55,8 +67,15 @@ export const RohlikModal: FC<Props> = ({
             src={`https://www.rohlik.cz/${rohlikId}`}
             height="800px"
             width="100%"
-            style={{ borderRadius: "8px" }}
-            onLoad={(e) => modifyModal(e.target as HTMLIFrameElement)}
+            style={{
+              borderRadius: "8px",
+              visibility: "hidden",
+              border: "none",
+            }}
+            onLoad={(e) => {
+              modifyModal(e.target as HTMLIFrameElement);
+              (e.target as HTMLIFrameElement).style.visibility = "visible";
+            }}
           />
         </div>
       </div>
