ca2eb4b185e732b0ab214ad12613cba6fd1b4c74
Mczella <markaczova@gmail.com>
2024-01-21 21:40:46 +0100

    Renamed import

--- src/components/Rohlik.tsx
+++ src/components/Rohlik.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavigationArea from "./NavigationArea.tsx";
 import MainArea from "./MainArea.tsx";
 import CartArea from "./cart/CartArea.tsx";
-import { UserArea } from "./UserSite/UserArea.tsx";
+import { UserArea } from "./userSite/UserArea.tsx";
 
 export const Rohlik = (): React.ReactNode => (
   <>
